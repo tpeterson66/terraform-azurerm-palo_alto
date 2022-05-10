296968625d5f59b15de78620801e01bde1988cf9
Tom Peterson <tpeterson@Toms-Mac-mini.local>
2022-05-10 16:56:26 -0400

    updates to the module adding CI/CD

---
+++ modules/network_security_group/.test/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+variable "buildId" {
+  default = "111111"
+  description = "Azure DevOps Build ID"
+  type = string
+}
+
+resource "azurerm_resource_group" "testing" {
+  name     = "automated_testing_${var.buildId}"
+  location = "East US"
+}
+
+module "network_security_group" {
+  source = "../"
+  name = "automated_testing_${var.buildId}"
+  resource_group = azurerm_resource_group.testing
+}
---
+++ modules/network_security_group/main.tf
@@ -0,0 +1,17 @@
+variable "name" {
+  type = string
+  description = "network security group name"
+}
+
+variable "resource_group" {
+  description = "Resource group object"
+}
+
+resource "azurerm_network_security_group" "this" {
+  name                = var.name
+  location            = var.resource_group.location
+  resource_group_name = var.resource_group.name
+  tags = {
+    environment = "Production"
+  }
+}
