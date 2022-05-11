596fc0210ed8514b2471c823397629a851b3c925
Tom Peterson <tpeterson@M-ALNVTP-1619.local>
2022-05-11 13:30:16 -0400

    updates to add route table

--- modules/route_table/main.tf
+++ modules/route_table/main.tf
@@ -8,8 +8,8 @@ variable "resource_group" {
 }
 
 resource "azurerm_route_table" "this" {
-  name                          = var.name
+  name                                    = var.name
   location                      = var.resource_group.location
-  resource_group_name           = var.resource_group.name
+  resource_group_name           =            var.resource_group.name
   disable_bgp_route_propagation = false
 }
