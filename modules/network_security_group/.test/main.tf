6627102f44c1e1cb2c5968bbfc0a5846b2333c94
Tom Peterson <tpeterson@M-ALNVTP-1619.local>
2022-05-11 09:16:45 -0400

    update to fix terraform format

--- modules/network_security_group/.test/main.tf
+++ modules/network_security_group/.test/main.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.5.0"
     }
   }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
 
 variable "buildId" {
-  default = "111111"
+  default     = "111111"
   description = "Azure DevOps Build ID"
-  type = string
+  type        = string
 }
 
 resource "azurerm_resource_group" "testing" {
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "testing" {
 }
 
 module "network_security_group" {
-  source = "../"
-  name = "automated_testing_${var.buildId}"
+  source         = "../"
+  name           = "automated_testing_${var.buildId}"
   resource_group = azurerm_resource_group.testing
 }
